d9dd3fbbf5e497eb256c2cc2dc02af2df6dfdfaf
NingAnMe <ninganme@qq.com>
2021-03-17 17:45:43 +0800

    cff转换

---
+++ mmsegmentation/tools/convert_datasets/cff.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+from sklearn.model_selection import train_test_split
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Convert Cityscapes annotations to TrainIds')
+    parser.add_argument('cff_path', help='cityscapes data path')
+    parser.add_argument('--gt-dir', default='gtFine', type=str)
+    parser.add_argument('-o', '--out-dir', help='output path')
+    parser.add_argument(
+        '--nproc', default=1, type=int, help='number of process')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    x = os.listdir(os.path.join(args.cff_path, 'img_train'))
+    y = [i.replace('jpg', 'png') for i in x]
+    x_train, x_s, y_train, y_s = train_test_split(x, y, test_size=0.4)
+    x_val, x_test, y_val, y_test = train_test_split(x_s, y_s, test_size=0.5)
+
+    for d in ['train', 'val', 'test']:
+        img_path = os.path.join(args.cff_path, 'img', d)
+        label_path = os.path.join(args.cff_path, 'label', d)
+        for d_path in [img_path, label_path]:
+            if not os.path.isdir(d_path):
+                os.makedirs(d_path)
+    for x, y in zip(x_train, y_train):
+        os.symlink(os.path.join(args.cff_train, 'img_train', x), os.path.join(args.cff_train, 'img', 'train'))
+        os.symlink(os.path.join(args.cff_train, 'lab_train', x), os.path.join(args.cff_train, 'label', 'train'))
+    
+    for x, y in zip(x_val, y_val):
+        os.symlink(os.path.join(args.cff_train, 'img_train', x), os.path.join(args.cff_train, 'img', 'val'))
+        os.symlink(os.path.join(args.cff_train, 'lab_train', x), os.path.join(args.cff_train, 'label', 'val'))
+    
+    for x, y in zip(x_test, y_test):
+        os.symlink(os.path.join(args.cff_train, 'img_train', x), os.path.join(args.cff_train, 'img', 'test'))
+        os.symlink(os.path.join(args.cff_train, 'lab_train', x), os.path.join(args.cff_train, 'label', 'test'))
+
+
+if __name__ == '__main__':
+    main()
